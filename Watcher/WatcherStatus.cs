ec2dc311fbbfb2d7599a3d1fbbf1f0d5acc42e19
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 20:26:11 +0900

    Create WatcherStatus.cs

---
+++ Watcher/WatcherStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FKP41
+{
+    public enum WatcherStatus
+    {
+        Unknown = 0,
+        /// <summary>
+        /// 処理中
+        /// </summary>
+        Processing = 102,
+        /// <summary>
+        /// リクエストを受け取ったが処理はされていない
+        /// </summary>
+        Accepted = 202,
+        /// <summary>
+        /// 権限が無いファイルやフォルダ
+        /// </summary>
+        Forbidden = 403,
+        /// <summary>
+        /// ファイルやフォルダが見つからない
+        /// </summary>
+        NotFound = 404,
+        /// <summary>
+        /// ティーポットでコーヒーを淹れようとする試みを拒否
+        /// </summary>
+        I_am_a_teapot = 418
+    }
+}
