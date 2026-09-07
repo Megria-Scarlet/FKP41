00a0ea0ceb34b572eaf4c10e293c4b7dafdd3d11
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 21:55:34 +0900

    Update IWatcher.cs

--- Watcher/IWatcher.cs
+++ Watcher/IWatcher.cs
@@ -9,6 +9,8 @@ namespace FKP41
     {
         public string FilePath { get; }
         public WatcherStatus Status { get; }
+        public DateTime? LastBackupTime { get; }
+        public uint FileCount { get; }
         public void OnUpdateStatus();
     }
 }
