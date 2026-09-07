de8198ec14c3a8b37a602b9a68f9d99899a65006
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 20:06:10 +0900

    Add Watcher

---
+++ Watcher/IWatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FKP41
+{
+    public interface IWatcher
+    {
+        public string FilePath { get; }
+    }
+}
---
+++ Watcher/WatcherViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FKP41
+{
+    internal class WatcherViewModel
+    {
+    }
+}
