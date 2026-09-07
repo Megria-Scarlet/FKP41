b0110eb9793ad6c7aa41e5ee5c8f0a080a5cc8ac
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 20:35:16 +0900

    Update WatcherViewModel.cs

--- Watcher/WatcherViewModel.cs
+++ Watcher/WatcherViewModel.cs
@@ -6,9 +6,11 @@ using System.Text;
 
 namespace FKP41
 {
-    public class WatcherViewModel : INotifyPropertyChanged
+    public class WatcherViewModel : INotifyPropertyChanged, IDisposable
     {
         private IWatcher _watcher;
+        private bool disposedValue;
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public string FilePath
@@ -20,14 +22,53 @@ namespace FKP41
         public WatcherViewModel(IWatcher watcher)
         {
             this._watcher = watcher;
+            this._watcher.PropertyChanged += OnPropertyChanged;
         }
 
         // This method is called by the Set accessor of each property.
         // The CallerMemberName attribute that is applied to the optional propertyName
         // parameter causes the property name of the caller to be substituted as an argument.
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+
+        }
+
+        #region Dispose
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                }
+                _watcher.PropertyChanged -= OnPropertyChanged;
+                _watcher = null!;
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        ~WatcherViewModel()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
