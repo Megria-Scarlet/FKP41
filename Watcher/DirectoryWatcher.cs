c2b45dcc0e6294203a7de922814247e0f87ed3ae
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 20:26:08 +0900

    Update DirectoryWatcher.cs

--- Watcher/DirectoryWatcher.cs
+++ Watcher/DirectoryWatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -9,14 +10,42 @@ namespace FKP41
     internal class DirectoryWatcher : IWatcher
     {
         private DirectoryInfo _directory;
+        public event PropertyChangedEventHandler? PropertyChanged;
         public string FilePath
         {
             [MethodImpl(MethodImplOptions.NoInlining)]
             get => _directory.FullName;
         }
+        private WatcherStatus status;
+        public WatcherStatus Status
+        {
+            get
+            {
+                bool token = false;
+                try
+                {
+                    spinLock.TryEnter(1000, ref token);
+                    return this.status;
+                }
+                finally
+                {
+                    if (token) spinLock.Exit();
+                }
+            }
+        }
+        private SpinLock spinLock;
+
         public DirectoryWatcher(string path)
         {
             _directory = new(path);
+            spinLock = new SpinLock();
+        }
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
