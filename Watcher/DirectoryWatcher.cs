3f1a7c77ee18c2e7643f54d65ebeed90f8b7054c
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 20:07:34 +0900

    Update DirectoryWatcher.cs

--- Watcher/DirectoryWatcher.cs
+++ Watcher/DirectoryWatcher.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace FKP41
 {
-    internal class DirectoryWatcher
+    internal class DirectoryWatcher : IWatcher
     {
+        private DirectoryInfo _directory;
+        public string FilePath
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            get => _directory.FullName;
+        }
+        public DirectoryWatcher(string path)
+        {
+            _directory = new(path);
+        }
     }
 }
