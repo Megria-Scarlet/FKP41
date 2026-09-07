43fa1d7baf6774f79a7b1cf268d8ff63827e578e
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 21:55:31 +0900

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,69 @@ namespace FKP41
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private System.Collections.ObjectModel.ObservableCollection<WatcherViewModel> watcherViewModels;
+        private System.Windows.Threading.DispatcherTimer renderTimer;
+        private System.Windows.Threading.DispatcherTimer statusTimer;
+        private Timer backupTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += OnLoaded;
+            watcherViewModels = [];
+            watcherViewModels =
+                [
+                    //new(new DirectoryWatcher(string.Empty)),
+                    new(new DirectoryWatcher(Environment.CurrentDirectory))
+                ];
+            renderTimer = new(System.Windows.Threading.DispatcherPriority.Render)
+            {
+                Interval = TimeSpan.FromTicks(TimeSpan.TicksPerMillisecond * 10)
+            };
+            renderTimer.Tick += OnRenderUpdate;
+            statusTimer = new(System.Windows.Threading.DispatcherPriority.Background)
+            {
+                Interval = TimeSpan.FromTicks(TimeSpan.TicksPerMinute)
+            };
+            statusTimer.Tick += OnStatusUpdate;
+            backupTimer = new(OnAutoBackup);
+        }
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.WatcherListView.ItemsSource = watcherViewModels;
+            this.renderTimer.Start();
+            this.backupTimer.Change(1000, 1000);
+
+            OnStatusUpdate(sender, e);
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            renderTimer.Stop();
+            backupTimer.Dispose();
+        }
+        private void OnRenderUpdate(object? sender, EventArgs e)
+        {
+            foreach (var watcher in watcherViewModels)
+            {
+                watcher.OnUpdateNextBackupSpan();
+            }
+        }
+        private void OnStatusUpdate(object? sender, EventArgs e)
+        {
+            foreach (var watcher in watcherViewModels)
+            {
+                watcher.OnUpdateStatus();
+            }
+        }
+        private void OnAutoBackup(object? state)
+        {
+            Timer? timer = (Timer?)state;
+            foreach (var watcher in watcherViewModels)
+            {
+
+            }
         }
     }
 }
