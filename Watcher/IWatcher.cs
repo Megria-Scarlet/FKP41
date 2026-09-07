f38f9b9383cdbfc205671c02fb09f8cc27ec03e0
Megria Scarlet <megriascarlet@gmail.com>
2026-09-07 20:26:10 +0900

    Update IWatcher.cs

--- Watcher/IWatcher.cs
+++ Watcher/IWatcher.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace FKP41
 {
-    public interface IWatcher
+    public interface IWatcher : INotifyPropertyChanged
     {
         public string FilePath { get; }
+        public WatcherStatus Status { get; }
     }
 }
