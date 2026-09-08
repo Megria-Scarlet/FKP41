0b70a410dfd2aed9723bd429e7d99e78a018485c
Megria Scarlet <megriascarlet@gmail.com>
2026-09-08 17:25:37 +0900

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,19 @@ namespace FKP41
     /// </summary>
     public partial class App : Application
     {
+        private static string dllFilePath = string.Empty;
+        public static string DllFilePath
+        {
+            [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+            get => dllFilePath;
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string exePath = Environment.GetCommandLineArgs()[0];
+            App.dllFilePath = System.IO.Path.GetFullPath(exePath);
+        }
     }
 
 }
