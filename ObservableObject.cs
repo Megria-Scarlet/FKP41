e10829a96e443bfc44883ae0dc3cae7a7b14ba08
Megria Scarlet <megriascarlet@gmail.com>
2026-09-08 18:06:58 +0900

    Update ObservableObject.cs

--- ObservableObject.cs
+++ ObservableObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -10,6 +11,7 @@ namespace FKP41
     /// 単一の <typeparamref name="T"/> 型のオブジェクトの変更通知をサポートするクラス。
     /// </summary>
     /// <typeparam name="T">任意の型。</typeparam>
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
     public class ObservableObject<T> : INotifyPropertyChanged
     {
         protected T? value;
@@ -21,6 +23,12 @@ namespace FKP41
             set => SetValue(value);
         }
         public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        public ObservableObject(in T value)
+        {
+            this.value = value;
+        }
 
         // This method is called by the Set accessor of each property.
         // The CallerMemberName attribute that is applied to the optional propertyName
@@ -44,6 +52,12 @@ namespace FKP41
             value = obj;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ref readonly T AsValue() => ref value!;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator T(ObservableObject<T> @object) => @object.value!;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override string? ToString() => value is string s ? s : (value?.ToString());
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private string GetDebuggerDisplay() => ToString() ?? "null";
     }
 }
