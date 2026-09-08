23d0f2bcec8fb6d7759f4ec6aa3533d478b88d1b
Megria Scarlet <megriascarlet@gmail.com>
2026-09-08 19:45:55 +0900

    Create TimeSpanConveter.cs

---
+++ TimeSpanConveter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace FKP41
+{
+    public class TimeSpanConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan timeSpan = (TimeSpan)value;
+            if (timeSpan.TotalMinutes >= 10)
+            {
+                return $"{(uint)timeSpan.TotalMinutes} min";
+            }
+            else if (timeSpan.TotalMinutes >= 1)
+            {
+                return $"{timeSpan.TotalMinutes.ToString("F1", culture)} min";
+            }
+            else if (timeSpan.TotalSeconds < 1)
+            {
+                return $"{timeSpan.TotalMilliseconds.ToString("F0", culture)} ms";
+            }
+            else
+            {
+                return $"{timeSpan.TotalSeconds.ToString("F1", culture)} s";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
