8474334e5c3d98437efa53bca2797f17c4639d86
Megria Scarlet <megriascarlet@gmail.com>
2026-09-08 17:48:06 +0900

    Create ObservableObject.cs

---
+++ ObservableObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FKP41
+{
+    /// <summary>
+    /// 単一の <typeparamref name="T"/> 型のオブジェクトの変更通知をサポートするクラス。
+    /// </summary>
+    /// <typeparam name="T">任意の型。</typeparam>
+    public class ObservableObject<T> : INotifyPropertyChanged
+    {
+        protected T? value;
+        public T Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => value!;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => SetValue(value);
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SetValue(in T obj)
+        {
+            if ((!typeof(T).IsValueType && !ReferenceEquals(obj, value)) || !EqualityComparer<T>.Default.Equals(value, obj))
+            {
+                value = obj;
+                NotifyPropertyChanged(nameof(Value));
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetValueNonNotification(in T obj)
+        {
+            value = obj;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator T(ObservableObject<T> @object) => @object.value!;
+    }
+}
